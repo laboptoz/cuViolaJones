dc47aab3922ae5925885002985898c6b7bb5297c
Nathan R Godwin <ngodwin@UCSD.EDU>
2018-06-04 18:53:17 -0700

    Working opencv haar cascade classifier

--- cpuViolaJones.hpp
+++ cpuViolaJones.hpp
@@ -1,5 +1,21 @@
 #include <opencv2/opencv.hpp>
 
-void cpuViolaJones(/*Mat * faces, Mat * nonfaces, float * err*/) {
-	printf("hey\n");
+using namespace cv;
+void cpuViolaJones(Mat face) {
+	String face_cascade_path = "C:/Users/ngodwin/Desktop/class_labs/Src/cuViolaJones/lib/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml";
+	CascadeClassifier face_cascade; 
+	if (!face_cascade.load(face_cascade_path)) {
+		printf("Error loading face cascade");
+	}
+	Mat gray_face;
+	cvtColor(face, gray_face, CV_BGR2GRAY);
+	equalizeHist(gray_face, gray_face);
+	std::vector<Rect> faces;
+	face_cascade.detectMultiScale(gray_face, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+	for (int i = 0; i < faces.size(); i++) {
+		rectangle(face, faces[i], Scalar(255, 0, 0),3);
+	}
+
+	imshow("Face result", face);
 }
--- cuViolaJones.cu
+++ cuViolaJones.cu
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "cpuViolaJones.hpp"
 
 using namespace cv;
+
 
 int main(int argc, char** argv )
 {
     Mat image;
-    image = imread("C:/Users/ngodwin/Desktop/class_labs/Src/cuViolaJones/image.jpg", 1 );
-
+    image = imread("C:/Users/ngodwin/Desktop/class_labs/Src/cuViolaJones/faces.jpg", 1 );
+	Mat smallimage;
+	resize(image, smallimage, Size(), 0.75, 0.75);
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
 
+	cpuViolaJones(image);
     waitKey(0);
 
     return 0;
