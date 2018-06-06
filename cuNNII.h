067012cb23b31a9c5c2cd56f86fa4c3766baf0bb
Nathan R Godwin <ngodwin@UCSD.EDU>
2018-06-06 14:43:44 -0700

    Working NN resize and row-scan

--- cuNNII.h
+++ cuNNII.h
@@ -1,90 +1,114 @@
 #include <cuda_runtime.h>
 
 __device__ void rowScan(float * input, unsigned int width);
+__inline__ __device__ unsigned int smallPow2(unsigned int width);
 
-__global__ void downsampleAndRow(unsigned char * input, float * output, unsigned int width, unsigned int scale) {
-	unsigned int id = threadIdx.x;
+__global__ void downsampleAndRow(unsigned char * input, float * output, unsigned int width, float scale) {
+	unsigned int idx = threadIdx.x;
+	unsigned int row = blockIdx.y;
+	unsigned int row_scaled = (unsigned int)(round(scale*blockIdx.y));
+	unsigned int width_scaled = round(width / scale);
+	unsigned int idx_scaled = (unsigned int)(round(scale*idx) + blockIdx.x*1024);
+
 	extern __shared__ float smem[];
-
-	if(scale < 1){
-		printf("Error: scale must be 1 =< scale");
-	}else if (scale == 1){
-		//MOVE TO SHARED MEMORY
-	}else{
-		smem[id] = (float)input[id*scale + blockIdx.x*width*scale];
-		__syncthreads();
-		rowScan(smem, width / scale);
-		smem[id] += (float)input[id*scale + blockIdx.x*width*scale];
-		output[id + blockIdx.x*(width / scale)] = smem[id];
-	}
+	smem[idx] = (float) input[idx_scaled + row_scaled*width];
+	smem[width_scaled + idx] = smem[idx];
+	rowScan(smem, width_scaled);
+	output[idx+blockIdx.y*width_scaled] = smem[width_scaled + idx];
 }
 
 __device__ void rowScan(float * input, unsigned int width) {
 	unsigned int idx = threadIdx.x;
-	int offset = 1;
-	//upsweep
-
-	for (int d = width >> 1; d > 0; d >>= 1) {
-		__syncthreads();
-		if (idx < d) {
-			int ai = offset * (2 * idx + 1) - 1;
-			int bi = offset * (2 * idx + 2) - 1;
-			input[bi] += input[ai];
+	unsigned int width_offset = 0;
+	unsigned int remainder = 0;
+	while (width_offset < width) {
+		unsigned int offset = 1;
+		unsigned int pw2 = smallPow2(width-width_offset);
+		unsigned int working_rem = remainder;
+		if (pw2 != 1) {
+			for (int i = pw2 / 2; i > 0; i /= 2) {
+				__syncthreads();
+				if (idx < i) {
+					input[width + offset * (2 * idx + 2) - 1 + width_offset] += input[width + offset * (2 * idx + 1) - 1 + width_offset];
+				}
+				offset *= 2;
+			}
+			if (idx == 0) {
+				remainder += input[width + pw2 + width_offset - 1];
+				input[width + pw2 + width_offset - 1] = 0;
+			}
+			for (int i = 1; i < pw2; i *= 2) {
+				offset /= 2;
+				__syncthreads();
+				if (idx < i) {
+					float swap = input[width + offset*(2 * idx + 1) - 1 + width_offset];
+					input[width + offset*(2 * idx + 1) - 1 + width_offset] = input[width + offset*(2 * idx + 2) - 1 + width_offset];
+					input[width + offset*(2 * idx + 2) - 1 + width_offset] = swap;
+				}
+			}
+			if (idx < pw2) {
+				input[width + idx + width_offset] += input[idx + width_offset] + working_rem;
+			}
+		} else {
+			if (idx < pw2) {
+				input[width + idx + width_offset] = input[idx + width_offset] + working_rem;
+			}
 		}
-		offset *= 2;
+		__syncthreads();
+		width_offset += pw2;
 	}
+	
+}
 
-	//Downsweep
-	if (idx == 0) input[width - 1] = 0;
-	for (int d = 1; d < width; d *= 2) {
-		offset >>= 1;
-		__syncthreads();
-		if (idx < d) {
-			int ai = offset * (2 * idx + 1) - 1;
-			int bi = offset * (2 * idx + 2) - 1;
-
-			float t = input[ai];
-			input[ai] = input[bi];
-			input[bi] += t;
+__global__ void colScan(float * in_arr, unsigned int height, unsigned int width) {
+	extern __shared__ float input[];
+	unsigned int idx = threadIdx.x;
+	unsigned int width_offset = 0;
+	unsigned int remainder = 0;
+	while (width_offset < width) {
+		unsigned int offset = 1;
+		unsigned int pw2 = smallPow2(width - width_offset);
+		unsigned int working_rem = remainder;
+		if (pw2 != 1) {
+			for (int i = pw2 / 2; i > 0; i /= 2) {
+				__syncthreads();
+				if (idx < i) {
+					input[width + offset * (2 * idx + 2) - 1 + width_offset] += input[width + offset * (2 * idx + 1) - 1 + width_offset];
+				}
+				offset *= 2;
+			}
+			if (idx == 0) {
+				remainder += input[width + pw2 + width_offset - 1];
+				input[width + pw2 + width_offset - 1] = 0;
+			}
+			for (int i = 1; i < pw2; i *= 2) {
+				offset /= 2;
+				__syncthreads();
+				if (idx < i) {
+					float swap = input[width + offset*(2 * idx + 1) - 1 + width_offset];
+					input[width + offset*(2 * idx + 1) - 1 + width_offset] = input[width + offset*(2 * idx + 2) - 1 + width_offset];
+					input[width + offset*(2 * idx + 2) - 1 + width_offset] = swap;
+				}
+			}
+			if (idx < pw2) {
+				input[width + idx + width_offset] += input[idx + width_offset] + working_rem;
+			}
 		}
+		else {
+			if (idx < pw2) {
+				input[width + idx + width_offset] = input[idx + width_offset] + working_rem;
+			}
+		}
+		__syncthreads();
+		width_offset += pw2;
 	}
 }
 
-__global__ void colScan(float * input, float * output, unsigned int height, unsigned int width) {
-	unsigned int idx = threadIdx.x;
-	extern __shared__ float smem[];
-	smem[idx] = input[idx*width + blockIdx.x];
-	__syncthreads();
-	int offset = 1;
-	//upsweep
-
-	for (int d = height >> 1; d > 0; d >>= 1) {
-		__syncthreads();
-		if (idx < d) {
-			int ai = offset * (2 * idx + 1) - 1;
-			int bi = offset * (2 * idx + 2) - 1;
-			smem[bi] += smem[ai];
-		}
-		offset *= 2;
-	}
-
-	//Downsweep
-	if (idx == 0) smem[height - 1] = 0;
-	for (int d = 1; d < height; d *= 2) {
-		offset >>= 1;
-		__syncthreads();
-		if (idx < d) {
-			int ai = offset * (2 * idx + 1) - 1;
-			int bi = offset * (2 * idx + 2) - 1;
-
-			float t = smem[ai];
-			smem[ai] = smem[bi];
-			smem[bi] += t;
-		}
-	}
-
-	__syncthreads();
-	//smem[idx] += input[idx*width + blockIdx.x];
-	output[idx*width + blockIdx.x] = smem[idx];
-
+__inline__ __device__ unsigned int smallPow2(unsigned int width) {
+	width = width | (width >> 1);
+	width = width | (width >> 2);
+	width = width | (width >> 4);
+	width = width | (width >> 8);
+	width = width | (width >> 16);
+	return width - (width >> 1);
 }
--- gpuViolaJones.cuh
+++ gpuViolaJones.cuh
@@ -14,36 +14,36 @@ inline void err_check(cudaError_t code, const char *file, int line, bool abort =
 
 
 void gpuViolaJones() {
-	int height = 20;
-	int width = 10;
-	int scale = 2;
-	if (width / scale % 2 != 0) {
-		width -= scale;
-	}
-	if (height / scale % 2 != 0) {
-		height -= scale;
-	}
+	int height = 8;
+	int width = 4;
+	float scale = 1.2;
+
 	unsigned char * input = new unsigned char[height*width];
 	for (int i = 0; i < height*width; i++) {
 		input[i] = i+1;
 	}
-
+	unsigned int scaled_height = round(height / scale);
+	unsigned int scaled_width = round(width / scale);
 	unsigned char * img_gpu;
 	float * ii_gpu;
-	float * ii_cpu = (float *)malloc(sizeof(float) * (width/scale)*(height/scale));
+	float * ii_cpu = (float *)malloc(sizeof(float) * scaled_height * scaled_width);
+
 	CHECK(cudaMalloc(&img_gpu, sizeof(unsigned char)*height*width));
-	CHECK(cudaMalloc(&ii_gpu, sizeof(float) * (width / scale)*(height / scale)));
+	CHECK(cudaMalloc(&ii_gpu, sizeof(float)  * scaled_height * scaled_width));
+
 	CHECK(cudaMemcpy(img_gpu, input, sizeof(unsigned char)*height*width, cudaMemcpyHostToDevice));
-	downsampleAndRow<<<height / scale, width / scale, width/scale*sizeof(float)>>> (img_gpu, ii_gpu, width, scale);
-	CHECK(cudaDeviceSynchronize());
+	
+	dim3 block((unsigned int)scaled_width);
+	dim3 grid((width + 1023) / 1024, scaled_height);
 
-	colScan <<<width / scale, height / scale, height / scale * sizeof(float) >> > (ii_gpu, ii_gpu, height / scale, width / scale);
+	downsampleAndRow<<<grid, block, 2* scaled_width *sizeof(float)>>> (img_gpu, ii_gpu, width, scale);
 	CHECK(cudaDeviceSynchronize());
-
-	CHECK(cudaMemcpy(ii_cpu, ii_gpu, sizeof(float) * (width / scale) * (height / scale), cudaMemcpyDeviceToHost));
-	for (int i = 0; i < height/scale; i++) {
-		for (int j = 0; j < width/scale; j++) {
-			printf("%f, ", ii_cpu[i*width/scale+j]);
+	
+	CHECK(cudaMemcpy(ii_cpu, ii_gpu, sizeof(float) * scaled_width * scaled_height, cudaMemcpyDeviceToHost));
+	
+	for (int i = 0; i < scaled_height; i++) {
+		for (int j = 0; j < scaled_width; j++) {
+			printf("%f\t", ii_cpu[i*scaled_width + j]);
 		}
 		printf("\n");
 	}
