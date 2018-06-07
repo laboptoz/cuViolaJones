cba52f368edd220a4e7256d20c0f5cb16242063f
xericho <xeric.hox@gmail.com>
2018-06-07 01:17:38 -0700

    Added paths for label.txt and images

--- paths.hpp
+++ paths.hpp
@@ -2,3 +2,9 @@
 #define CASCADE_PATH "./data/haarcascade_frontalface_default.xml"
 #define INFO_PATH "./data/info.txt"
 #define CLASS_PATH "./data/class.txt"
+#define LABEL_PATH "./data/labels.txt"
+#define IMAGE_PATH "./data/images/"    // images should go in data folder!
+
+// Converts a macro to a string
+#define STR1(x)  #x
+#define STR(x)  STR1(x)
