39268ba42bcf170c290dea758ade505dd4a3d185
Nathan R Godwin <ngodwin@UCSD.EDU>
2018-06-08 23:42:38 -0700

    Added partial work on alternate method

--- paths.hpp
+++ paths.hpp
@@ -1,4 +1,4 @@
-#define FACE_PATH "./data/notface.jpg"
+#define FACE_PATH "./data/faces.jpg"
 #define CASCADE_PATH "./data/haarcascade_frontalface_default.xml"
 #define INFO_PATH "./data/info.txt"
 #define CLASS_PATH "./data/class.txt"
