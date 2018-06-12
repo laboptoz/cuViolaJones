20dc4ee7695b8600f4bb4f30008487d9f2f1c91b
Eric Ho <erho@UCSD.EDU>
2018-06-11 17:36:55 -0700

    working webcam code

--- cpuViolaJones.hpp
+++ cpuViolaJones.hpp
@@ -102,179 +102,69 @@ void testCpuViolaJones(Image * imgs, int numImgs, bool display) {
 	printf("Time elapsed: %.8lfs\n\n", (clock() - start) / (double)CLOCKS_PER_SEC);
 }
 
-float ** cpuIntegralImage(unsigned char * original,
-					 unsigned int ** sizes_ptr,
-		 			 unsigned int * depth,
-					 unsigned int min_size,
-					 unsigned int width,
-					 unsigned int height,
-					 float scale) {
-	//DETERMINE PYRAMID DEPTH
-	unsigned int scaled_width = width;
-	unsigned int scaled_height = height;
-	*depth = 0;
-	unsigned int sum_size = 0;
 
-	if (min_size <= 3) {
-		min_size = 4;
-	}
-	while (scaled_width >= min_size && scaled_height >= min_size) {
-		(*depth)++;
-		sum_size += scaled_width*scaled_height;
-		scaled_width = round(scaled_width / scale);
-		scaled_height = round(scaled_height / scale);
-	}
-	printf("Image pyramid depth is: %u\n", *depth);
-
-	// Malloc the images
-	float * imagePyramid = new float[sum_size * sizeof(float)];
-
-	float ** integralimages = new float *[*depth];
-
-	//Assign pyramid sizes to argument 'sizes'
-	sum_size = 0;
-	scaled_width = width;
-	scaled_height = height;
-	*sizes_ptr = (unsigned int *)malloc(2 * (*depth) * sizeof(unsigned int)); // 2 b/c width and height
-	unsigned int * sizes = *sizes_ptr;
-	for (int i = 0; i < *depth; i++) {
-		integralimages[i] = imagePyramid + sum_size;
-		sum_size += scaled_width*scaled_height;
-		sizes[2 * i] = scaled_width;
-		sizes[2 * i + 1] = scaled_height;
-		scaled_width = round(scaled_width / scale);
-		scaled_height = round(scaled_height / scale);
-	}
-
-	// Convert orignal image to a cv::Mat
-	Mat img = Mat(width, height, CV_8UC1, original);
-	cout << "Original: " << endl;
-	cout << img << endl << endl;
-
-	// First integral image
-	Mat integ, tmp;
-	tmp = img;
-	integral(img, tmp);
-	cout << "Pyramid level 0:" << endl;
-	cout << tmp << endl << endl;
-	//unsigned char * data = integ.data;
-
-
-	scaled_width = width;
-	scaled_height = height;
-	// Generate remaining integral images
-	for (int i = 0; i < *depth; i++) {
-		printf("Pyramid level: %u\n", i);
-		cout << scaled_width << " "<< scaled_height << endl;
-		// Downsample
-		Mat tmp1;
-		resize(img, tmp1, Size(scaled_width, scaled_height), 0, 0, CV_INTER_LINEAR);
-		// Integral image
-		Mat tmp2;
-		integral(tmp1, tmp2);
-		cout << tmp2 << endl;
-		Mat img = tmp2.clone();
-		// Change size
-		scaled_width = round(scaled_width / scale);
-		scaled_height = round(scaled_height / scale);
-		// Save data
-		//float *idx = integralimages[i];
-		//memcpy(imagePyramid[idx], tmp2.data, scaled_width*scaled_height);
-	}
-
-	return integralimages;
-}
-
-void testCpuII() {
-	unsigned int width = 8;
-	unsigned int height = 8;
-	unsigned char * img = new unsigned char[width * height];
-	for (int i = 0; i < width * height; i++) {
-		img[i] = 1;
-	}
-	unsigned int win_size = 2;
-	float scale = 1.2;
-
-	unsigned int * iiPyramidSizes = nullptr;
-	unsigned int iiPyramidDepth = 0;
-	float ** integralImages = cpuIntegralImage(img,
-								 &iiPyramidSizes, 
-								 &iiPyramidDepth, 
-								 win_size, 
-								 width, 
-								 height, 
-								 scale);
-}
-
-/*
-void cpuWebcam(String face_cascade_path) {
+void cpuWebcam() {
 
 	cout << "Starting webcam..." << endl << endl;
 
 	// capture from web camera init
 	VideoCapture cap(0);
-	cout << "Camera found!" << endl;
+	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 720);
+	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
-	if (!cap.open(0)) {
-		cout << "Webcam not open" << endl;
+	CascadeClassifier face_cascade;
+	face_cascade.load(CASCADE_PATH);
+
+	cout << "Camera found!" << endl;
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
 		return;
 	}
-	for (;;) {
-		cout << "Starting loop" << endl;
-		Mat frame;
-		cap >> frame;
-		if (frame.empty()) {
-			cout << "Frame is empty" << endl;
-			break; // end of video stream
+
+	/*
+	while (1) {
+		cout << "Running.." << endl;
+		//Mat img;
+		//cap >> img;
+		//imshow("Window 1", img);
+
+		Mat img2;
+		bool success = cap.read(img2);
+		imshow(window_name, img2);
+
+		if (success == false) {
+			cout << "Video camera is disconnected" << endl;
+			break;
 		}
-		cout << "Displaying image" << endl;
-		imshow("this is you, smile! :)", frame);
-		if (waitKey(10) == 27) {
-			break; // stop capturing by pressing ESC 
+
+		char c = waitKey(10);
+		if (c == 'b') {
+			break; //break when b is pressed
 		}
 	}
-
-	
+	*/
 	Mat img;
-
-	// Initialize the inbuilt Harr Cascade frontal face detection
-	// Below mention the path of where your haarcascade_frontalface_alt2.xml file is located
-	CascadeClassifier face_cascade;
-	if (!face_cascade.load(face_cascade_path)) {
-		printf("Error loading face cascade");
-	}
-
-	for (;;) {
+	for (;;){
 
 		// Image from camera to Mat
-		cap >> img;
-
-		// obtain input image from source
-		cap.retrieve(img);
-
-		// Just resize input image if you want
-		resize(img, img, Size(1000, 640));
+		cap.read(img);
 
 		// Container of faces
 		vector<Rect> faces;
 
 		// Detect faces
-		face_cascade.detectMultiScale(img, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(140, 140));
-
-		//Show the results
-		// Draw circles on the detected faces
-		for (int i = 0; i < faces.size(); i++) {
-			Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
-			ellipse(img, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-		}
+		face_cascade.detectMultiScale(img, faces, SCALING, MIN_NEIGH, 0 | CV_HAAR_SCALE_IMAGE, Size(WIN_SIZE, WIN_SIZE));
 
 		// To draw rectangles around detected faces
 		for (unsigned i = 0; i<faces.size(); i++)
-			rectangle(img,faces[i], Scalar(255, 0, 0), 2, 1);
+			rectangle(img, faces[i], Scalar(255, 0, 0), 2); // BLUE
 
 		imshow("CPU", img);
-		int key2 = waitKey(20);
+		char c = waitKey(10);
+		if (c == 'b') {
+			break; //break when b is pressed
+		}
 
 	}
+	
 }
-*/
--- gpuViolaJones.cuh
+++ gpuViolaJones.cuh
@@ -81,6 +81,8 @@ void testGpuViolaJones(Image *faces, int numImgs, bool display) {
 	int *tp = new int, *fp = new int; 
 	*tp = *fp = 0;
 
+	MyImage imageObj;
+	MyImage *image = &imageObj;
 	clock_t start = clock();
 	for (int n = 0; n < numImgs; n++) {
 		if (PRINT)
@@ -89,13 +91,10 @@ void testGpuViolaJones(Image *faces, int numImgs, bool display) {
 		// Ground truth bbox
 		Rect gt = Rect(faces[n].x, faces[n].y, faces[n].w, faces[n].h);
 
-		MyImage imageObj;
-		MyImage *image = &imageObj;
 		image->data = faces[n].grayscale.data;
 		image->width = faces[n].grayscale.cols;
 		image->height = faces[n].grayscale.rows;
 		image->maxgrey = 255;
-		image->flag = 1;
 
 		std::vector<MyRect> result;
 		detect_faces(image->width, image->height, result, image, SCALING, MIN_NEIGH);
@@ -129,4 +128,122 @@ void testGpuViolaJones(Image *faces, int numImgs, bool display) {
 	printf("Final GPU accuracy: %d/%d = %f\n", *tp, numImgs, (float)*tp / numImgs);
 	printf("Final GPU false positives: %d/%d = %f\n", *fp, numImgs, (float)*fp / numImgs);
 	printf("Time elapsed: %.8lfs\n\n", (clock() - start) / (double)CLOCKS_PER_SEC);
+}
+
+
+void gpuWebcam() {
+
+	cout << "Starting webcam..." << endl << endl;
+
+	// capture from web camera init
+	VideoCapture cap(0);
+	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 720);
+	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+	cout << "Camera found!" << endl;
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return;
+	}
+
+	Mat img;
+	MyImage imageObj;
+	MyImage *image = &imageObj;
+	for (;;) {
+
+		// Image from camera to Mat
+		cap.read(img);
+
+		// Convert image to grayscale
+		Mat gray_face;
+		cvtColor(img, gray_face, CV_BGR2GRAY);
+		
+		image->data = gray_face.data;
+		image->width = gray_face.cols;
+		image->height = gray_face.rows;
+		image->maxgrey = 255;
+
+		std::vector<MyRect> result;
+		detect_faces(image->width, image->height, result, image, SCALING, MIN_NEIGH);
+
+		for (int i = 0; i < result.size(); i++) {
+			Rect pred = Rect(result[i].x, result[i].y, result[i].width, result[i].height);
+			rectangle(img, pred, Scalar(255, 0, 0), 2); // GREEN
+		}
+
+		imshow("GPU", img);
+		char c = waitKey(10);
+		if (c == 'b') {
+			break; //break when b is pressed
+		}
+
+	}
+
+}
+
+void webcam() {
+
+	cout << "Starting webcam..." << endl << endl;
+	cout << "GPU is RED. CPU is BLUE" << endl << endl;
+
+	// capture from web camera init
+	VideoCapture cap(0);
+	//cap.set(CV_CAP_PROP_FRAME_WIDTH, 720);
+	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+	cout << "Camera found!" << endl;
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return;
+	}
+
+	CascadeClassifier face_cascade;
+	face_cascade.load(CASCADE_PATH);
+
+	Mat img;
+	MyImage imageObj;
+	MyImage *image = &imageObj;
+	for (;;) {
+
+		// Image from camera to Mat
+		cap.read(img);
+
+		// GPU CODE
+		Mat gray_face;
+		cvtColor(img, gray_face, CV_BGR2GRAY);
+
+		image->data = gray_face.data;
+		image->width = gray_face.cols;
+		image->height = gray_face.rows;
+		image->maxgrey = 255;
+
+		std::vector<MyRect> result;
+		detect_faces(image->width, image->height, result, image, SCALING, MIN_NEIGH);
+
+		for (int i = 0; i < result.size(); i++) {
+			Rect pred = Rect(result[i].x, result[i].y, result[i].width, result[i].height);
+			rectangle(img, pred, Scalar(0, 0, 255), 2); // RED
+		}
+
+		// CPU CODE
+		// Container of faces
+		vector<Rect> faces;
+
+		// Detect faces
+		face_cascade.detectMultiScale(img, faces, SCALING, MIN_NEIGH, 0 | CV_HAAR_SCALE_IMAGE, Size(WIN_SIZE, WIN_SIZE));
+
+		// To draw rectangles around detected faces
+		for (unsigned i = 0; i<faces.size(); i++)
+			rectangle(img, faces[i], Scalar(255, 0, 0), 2); // BLUE
+
+
+
+		imshow("CPU and GPU", img);
+		char c = waitKey(10);
+		if (c == 'b') {
+			break; //break when b is pressed
+		}
+
+	}
+
 }
