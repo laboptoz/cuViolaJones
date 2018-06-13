c2675a846af9269699b48c93e37672f2471b35b7
Kenmei <1996brianhnenriquez@gmail.com>
2018-06-12 19:24:53 -0700

    add comments

--- haar.cuh
+++ haar.cuh
@@ -1,14 +1,9 @@
 #pragma once
 #include <cuda_runtime.h>
-//#include "cuIntegralImage.cuh"
-//#include "cuNNII.h"
-
-#include "haar.h"
+#include <cuda.h>
 #include <stdio.h>
 #include "paths.hpp"
-
-#include <cuda.h>
-
+#include "haar.h"
 #include "cuNNII_v2.cuh"
 #include "Filter.cuh"
 //#include "cuda_error_check.h"
@@ -17,34 +12,21 @@
 #include <iostream>
 #include <fstream>
 
-/* compute integral images */
+// compute integral images
 void integralImages_cpp(ImageUnion *src, ImageUnion *sum, ImageUnion *sqsum);
 
-/* compute scaled image */
+// compute down-scaled image
 void nearestNeighbor_cpp(ImageUnion *src, ImageUnion *dst);
 
-/* rounding function */
-inline  int  myRound_cpp(float value)
-{
+// rounding function
+inline  int  myRound_cpp(float value){
 	return (int)(value + (value >= 0 ? 0.5 : -0.5));
 }
 
-/***********************************************
-* Note:
-* The int_sqrt is softwar integer squre root.
-* GPU has hardware for floating squre root (sqrtf).
-* In GPU, it is wise to convert an int variable
-* into floating point, and use HW sqrtf function.
-* More info:
-* http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#standard-functions
-**********************************************/
-/*****************************************************
-* The int_sqrt is only used in runCascadeClassifier
-* If you want to replace int_sqrt with HW sqrtf in GPU,
-* simple look into the runCascadeClassifier function.
-*****************************************************/
-__device__ unsigned int int_sqrt_cpp(unsigned int value)
-{
+/*******************************************************
+* Compute square root of integer value
+*******************************************************/
+__device__ unsigned int int_sqrt_cpp(unsigned int value) {
 	int i;
 	unsigned int a = 0, b = 0, c = 0;
 	for (i = 0; i < (32 >> 1); i++)
@@ -65,12 +47,15 @@ __device__ unsigned int int_sqrt_cpp(unsigned int value)
 	return a;
 }
 
-
+/*******************************************************
+* Perform cascade classifier
+*******************************************************/
 __device__ bool operation(unsigned int* sum, unsigned int* sqsum, unsigned int idx, 
 	unsigned int img_width, unsigned int wd_width, unsigned int wd_height,
-	Stage * stages_gpu) 
-{
-	// normalize image; 0 top left, 1 top right, 2 bottom left, 3 bottom right
+	Stage * stages_gpu) {
+
+	// normalize image by 1/std
+	// index 0 top left, 1 top right, 2 bottom left, 3 bottom right
 	unsigned int idx0 = idx;
 	unsigned int idx1 = idx + wd_width - 1;
 	unsigned int idx2 = idx + (wd_height - 1) * img_width;
@@ -82,89 +67,81 @@ __device__ bool operation(unsigned int* sum, unsigned int* sqsum, unsigned int i
 		norm_factor = int_sqrt_cpp(norm_factor);
 	else
 		norm_factor = 1;
-	//printf("i0 %d, i1 %d, i2 %d, i3 %d, sqsum %d, mean %d, norm_factor %d, area %d\n", idx0, idx1, idx2, idx3, wd_sqsum, wd_mean, norm_factor, wd_width * wd_height);
 
-	// run filter
+	// run stages
 	const int num_stages = 25;
-
 	int num_filters;
 	int stage_sum;
-
 	bool pass = false;
 	for (int i = 0; i < num_stages; i++) {
-
-		//printf("stage %d: ", i);
-
-		//printf("sums %d %d %d %d, norm factor %f, running filter, idx %d \n", *(sum + idx0), sum[idx1], sum[idx2], sum[idx3], norm_factor, idx);
 		pass = stages_gpu[i].getValue<unsigned int>(sum + idx0, norm_factor, img_width);
 
-		//break;
-
+		// warp level pruning
 		unsigned int mask = __ballot(pass == false);
 		unsigned int falseCount = __popc(mask);
 		if (falseCount > PRUNING)
 			pass = false;
-
 		if (!pass)
 			return false;
-
 	}
-	//printf("result.. ");
 	return true;
 }
 
+
+/******************************************************
+* Run sliding window step
+******************************************************/
 __global__ void slide_window(
 	unsigned int* sum1, unsigned int* sqsum1,
 	bool *d_activation,
 	unsigned int wd_width, unsigned int wd_height, 
 	unsigned int img_width, unsigned int img_height,
-	Stage * stages_gpu
+	Stage * stages_gpu) {
 
-) {
-	//printf("in slide window, ");
-	// get four corners
+	// get top left element's index
 	unsigned int topLeft_idx = blockIdx.y * img_width + blockIdx.x * blockDim.x + threadIdx.x;
-	//printf("r %d c %d, ", blockIdx.y, blockIdx.x * blockDim.x + threadIdx.x);
-	
-	//int result = test_sum(d_image, topLeft_idx, img_width, 1);
 	
 	// to differentiate if goes outside of image
 	if ((topLeft_idx + wd_width - 1) / img_width == blockIdx.y) {
-		bool result = operation(sum1, sqsum1, topLeft_idx, img_width, wd_width, wd_height, stages_gpu);
-		//printf("after operation, ");
 
+		// run cascade classifier
+		bool result = operation(sum1, sqsum1, topLeft_idx, img_width, wd_width, wd_height, stages_gpu);
+		
+		// return value to activation map
 		unsigned int dest_idx = blockIdx.y * (img_width - wd_width + 1) + blockIdx.x * blockDim.x + threadIdx.x;
-		//printf("%d ,", dest_idx);
 		d_activation[dest_idx] = result;
 	}
-	//printf("\n");
 }
 
+
+/******************************************************
+* Get integral images and run sliding window to detect faces
+******************************************************/
 void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<Rectangle> &allCandidates, 
 	ImageUnion* _img, float scale_factor, int minNeighbors, unsigned int * num_stages, Stage * stages_gpu) {
 
-	/* group overlaping windows */
+	// group overlaping windows
 	const float GROUP_EPS = 0.4f;
 
-	/* pointer to input image */
+	// pointer to input image
 	ImageUnion *img = _img;
 
-	/* pointers for the created structs */
+	// pointers for the created structs
 	ImageUnion *img1 = new ImageUnion();
 	ImageUnion *sum1 = new ImageUnion();
 	ImageUnion *sqsum1 = new ImageUnion();
 
-	/* malloc for img1: unsigned char */
+	// malloc for img1: unsigned char
 	img1->width = img->width;
 	img1->height = img->height;
 	img1->dataChar = (unsigned char *)malloc(sizeof(unsigned char)*(img->height*img->width));
 
-	/* malloc for sum1: unsigned char */
+	// malloc for sum1: unsigned char
 	sum1->width = img->width;
 	sum1->height = img->height;
 	sum1->dataInt = (int*)malloc(sizeof(int)*(img->height*img->width));
 
-	/* malloc for sqsum1: unsigned char */
+	// malloc for sqsum1: unsigned char
 	sqsum1->width = img->width;
 	sqsum1->height = img->height;
 	sqsum1->dataInt = (int*)malloc(sizeof(int)*(img->height*img->width));
@@ -191,8 +168,6 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 #endif
 
 	int wd_height = WIN_SIZE, wd_width = WIN_SIZE;
-	//scale_factor = 1;
-	//minNeighbors = 1;
 
 	int counter = 0;
 
@@ -201,18 +176,14 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 #elif GPUII == 0
 	for (int i = 0;;i++){
 #endif
-		/* size of the image scaled up */
+		// size of the image scaled up
 		ImageDim winSize = { myRound_cpp(wd_width*scale_factor), myRound_cpp(wd_height*scale_factor) };
-		/* size of the image scaled down (from bigger to smaller) */
+		// size of the image scaled down (from bigger to smaller)
 		ImageDim sz = { (img->width / scale_factor), (img->height / scale_factor) };
-		/* difference between sizes of the scaled image and the original detection window */
+		// difference between sizes of the scaled image and the original detection window
 		ImageDim sz1 = { sz.width - wd_width, sz.height - wd_height };
-		/* if the actual scaled image is smaller than the original detection window, break */
 
-		//if (sz1.width < 0 || sz1.height < 0)
-		//	break;
-
-
+		// if the actual scaled image is smaller than the original detection window, break
 		if (sz1.width < 0 || sz1.height < 0)
 			break;
 
@@ -274,6 +245,7 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 		unsigned int down_h = sizes[2 * i + 1];//sz.height;
 		unsigned int down_w = sizes[2 * i];// sz.width;
 #elif GPUII == 0
+		// copy cpu integral image to gpu
 		unsigned int* d_sum1;
 		unsigned int* d_sqsum1;
 		CHECK(cudaMalloc((void**)&d_sum1, sum1->width * sum1->height * sizeof(int)));
@@ -287,7 +259,6 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 
 		unsigned int num_wd_col = down_h - wd_height + 1;  // number of windows on a column
 		unsigned int num_wd_row = down_w - wd_width + 1;  // number of windows on a row
-		//printf("sum dim: %d * %d, activation dim: %d * %d\n", down_h, down_w, num_wd_col, num_wd_row);
 
 		// create activation map
 		unsigned int act_size = num_wd_col * num_wd_row * sizeof(bool);
@@ -305,7 +276,6 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 		CHECK(cudaMemcpy(h_activation, d_activation, act_size, cudaMemcpyDeviceToHost));
 
 		// add activations to vector
-		
 		for (unsigned int j = 0; j < num_wd_col * num_wd_row; j++) {
 			if (h_activation[j]) {
 				unsigned int y = j / num_wd_row;
@@ -345,16 +315,10 @@ void detect_faces(unsigned int img_width, unsigned int img_height, std::vector<R
 }
 
 
-
-
-/*****************************************************
-* Compute the integral image (and squared integral)
-* Integral image helps quickly sum up an area.
-* More info:
-* http://en.wikipedia.org/wiki/Summed_area_table
+/****************************************************
+* Compute the integral image on CPU
 ****************************************************/
-void integralImages_cpp(ImageUnion *src, ImageUnion *sum, ImageUnion *sqsum)
-{
+void integralImages_cpp(ImageUnion *src, ImageUnion *sum, ImageUnion *sqsum) {
 	int x, y, s, sq, t, tq;
 	unsigned char it;
 	int height = src->height;
@@ -362,22 +326,19 @@ void integralImages_cpp(ImageUnion *src, ImageUnion *sum, ImageUnion *sqsum)
 	unsigned char *data = src->dataChar;
 	int * sumData = sum->dataInt;
 	int * sqsumData = sqsum->dataInt;
-	for (y = 0; y < height; y++)
-	{
+	for (y = 0; y < height; y++) {
 		s = 0;
 		sq = 0;
-		/* loop over the number of columns */
-		for (x = 0; x < width; x++)
-		{
+		// loop over the number of columns
+		for (x = 0; x < width; x++) {
 			it = data[y*width + x];
-			/* sum of the current row (integer)*/
+			// sum of the current row (integer)
 			s += it;
 			sq += it*it;
-
+			// sum rows and cols above
 			t = s;
 			tq = sq;
-			if (y != 0)
-			{
+			if (y != 0) {
 				t += sumData[(y - 1)*width + x];
 				tq += sqsumData[(y - 1)*width + x];
 			}
@@ -388,12 +349,9 @@ void integralImages_cpp(ImageUnion *src, ImageUnion *sum, ImageUnion *sqsum)
 }
 
 /***********************************************************
-* This function downsample an image using nearest neighbor
-* It is used to build the image pyramid
+* Downscale an image using nearest neighbor
 **********************************************************/
-void nearestNeighbor_cpp(ImageUnion *src, ImageUnion *dst)
-{
-
+void nearestNeighbor_cpp(ImageUnion *src, ImageUnion *dst) {
 	int y;
 	int j;
 	int x;
@@ -404,24 +362,20 @@ void nearestNeighbor_cpp(ImageUnion *src, ImageUnion *dst)
 	int h1 = src->height;
 	int w2 = dst->width;
 	int h2 = dst->height;
-
 	int rat = 0;
 
 	unsigned char* src_data = src->dataChar;
 	unsigned char* dst_data = dst->dataChar;
 
-
 	int x_ratio = (int)((w1 << 16) / w2) + 1;
 	int y_ratio = (int)((h1 << 16) / h2) + 1;
 
-	for (i = 0; i<h2; i++)
-	{
+	for (i = 0; i<h2; i++) {
 		t = dst_data + i*w2;
 		y = ((i*y_ratio) >> 16);
 		p = src_data + y*w1;
 		rat = 0;
-		for (j = 0; j<w2; j++)
-		{
+		for (j = 0; j<w2; j++) {
 			x = (rat >> 16);
 			*t++ = p[x];
 			rat += x_ratio;
